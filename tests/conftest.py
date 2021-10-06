e4fee3e5e649574bef2f0b10d17da16381d3a5dd
Tonkers Kuma <tonkers.society@gmail.com>
2021-10-06 14:10:09 -0700

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -40,49 +40,76 @@ def keeper(accounts):
 
 @pytest.fixture
 def token():
-    token_address = "0xdac17f958d2ee523a2206206994597c13d831ec7"  # USDT
+    # token_address = "0xdac17f958d2ee523a2206206994597c13d831ec7"  # USDT
+    token_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"  # WETH
+    # token_address = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"  # WBTC
     # token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # DAI
-    # token_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"  # WETH
 
     yield Contract(token_address)
+
 
 @pytest.fixture
 def token_whale(accounts):
-    addr = "0xa929022c9107643515f5c777ce9a910f0d1e490c"  # USDT
-    # addr = "0x030ba81f1c18d280636f32af80b9aad02cf0854e"  # WETH
+    # addr = "0xa929022c9107643515f5c777ce9a910f0d1e490c"  # USDT
+    addr = "0x030ba81f1c18d280636f32af80b9aad02cf0854e"  # WETH
+    # addr = "0xccf4429db6322d5c611ee964527d42e5d685dd6a" # WBTC
+    # addr = "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643"  # DAI
     yield accounts.at(addr, force=True)
+
 
 @pytest.fixture
 def pool():
-    pool = "0xb1b225402b5ec977af8c721f42f21db5518785dc"  # USDT via Aave
-    # pool = "0xe344646a7E7985948518AB8755A3565bc9211753"  # WETH via Aave
+    # pool = "0xb1b225402b5ec977af8c721f42f21db5518785dc"  # USDT via Aave
+    pool = "0xaE5ddE7EA5c44b38c0bCcfb985c40006ED744EA6"  # WETH via Aave
+    # pool = "0xA0E78812E9cD3E754a83bbd74A3F1579b50436E8"  # WBTC via Compound
+    # pool = "0x4B4626c1265d22B71ded11920795A3c6127A0559"  # DAI via BProtocol
+
     yield pool
 
-@pytest.fixture
-def token2():
-    token_address = "0xdac17f958d2ee523a2206206994597c13d831ec7"  # USDT
-    # token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # DAI
-    # token_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"  # WETH
-
-    yield Contract(token_address)
-
-@pytest.fixture
-def token_whale2(accounts):
-    addr = "0xa929022c9107643515f5c777ce9a910f0d1e490c"  # USDT
-    # addr = "0x030ba81f1c18d280636f32af80b9aad02cf0854e"  # WETH
-    yield accounts.at(addr, force=True)
-
-@pytest.fixture
-def pool2():
-    pool = "0xb1b225402b5ec977af8c721f42f21db5518785dc"  # USDT via Aave
-    # pool = "0xe344646a7E7985948518AB8755A3565bc9211753"  # WETH via Aave
-    yield pool
 
 @pytest.fixture
 def amount(accounts, token, user, token_whale):
-    amount = 10_000_000 * 10 ** token.decimals()
+    amount = 10_000 * 10 ** token.decimals()
 
     token.transfer(user, amount, {"from": token_whale})
+    yield amount
+
+
+@pytest.fixture
+def token2():
+    # token_address = "0xdac17f958d2ee523a2206206994597c13d831ec7"  # USDT
+    # token_address = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"  # WETH
+    # token_address = "0xccf4429db6322d5c611ee964527d42e5d685dd6a"  # WBTC
+    token_address = "0x6b175474e89094c44da98b954eedeac495271d0f"  # DAI
+
+    yield Contract(token_address)
+
+
+@pytest.fixture
+def token_whale2(accounts):
+    # addr = "0xa929022c9107643515f5c777ce9a910f0d1e490c"  # USDT
+    # addr = "0x030ba81f1c18d280636f32af80b9aad02cf0854e"  # WETH
+    # addr = "0xccf4429db6322d5c611ee964527d42e5d685dd6a"  # WBTC
+    addr = "0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643"  # DAI
+
+    yield accounts.at(addr, force=True)
+
+
+@pytest.fixture
+def pool2():
+    # pool = "0xb1b225402b5ec977af8c721f42f21db5518785dc"  # USDT via Aave
+    # pool = "0xaE5ddE7EA5c44b38c0bCcfb985c40006ED744EA6"  # WETH via Aave
+    # pool = "0xA0E78812E9cD3E754a83bbd74A3F1579b50436E8"  # WBTC via Compound
+    pool = "0x4B4626c1265d22B71ded11920795A3c6127A0559"  # DAI via BProtocol
+
+    yield pool
+
+
+@pytest.fixture
+def amount2(accounts, token2, user, token_whale2):
+    amount = 100_000_000 * 10 ** token2.decimals()
+
+    token2.transfer(user, amount, {"from": token_whale2})
     yield amount
 
 
@@ -97,7 +124,6 @@ def weth_amout(user, weth):
     weth_amout = 10 ** weth.decimals()
     user.transfer(weth, weth_amout)
     yield weth_amout
-
 
 
 @pytest.fixture
@@ -146,6 +172,8 @@ def percentageFeeModel():
 def strategy(strategist, keeper, vault, Strategy, gov, pool, stakeToken, bancorRegistry):
     strategy = strategist.deploy(Strategy, vault, pool, stakeToken, bancorRegistry)
     strategy.setKeeper(keeper)
+    strategy.setMinWithdraw(1e9, {'from': gov})
+    strategy.setDust(1e6, {'from': gov})
     vault.addStrategy(strategy, 10_000, 0, 2 ** 256 - 1, 1_000, {"from": gov})
     yield strategy
 
